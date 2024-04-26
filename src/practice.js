e01cab23b72450aac41beae1e97eb41af4308317
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-04-26 10:33:39 -0500

    updated tests

--- src/practice.js
+++ src/practice.js
@@ -6,4 +6,24 @@ function capitalize(str) {
 	return arr.join("");
 }
 
-module.exports = capitalize;
+function reverseString(str) {
+	let newStr = str.split("").reverse().join("");
+	return newStr;
+}
+
+const calculator = {
+	add: (x, y) => {
+		return x + y;
+	},
+	subtract: (x, y) => {
+		return x - y;
+	},
+	multiply: (x, y) => {
+		return x * y;
+	},
+	divide: (x, y) => {
+		return x / y;
+	},
+};
+
+module.exports = { capitalize, reverseString, calculator };
--- tests/practice.test.js
+++ tests/practice.test.js
@@ -1,9 +1,30 @@
-const capitalize = require("../src/practice");
+const { capitalize, reverseString, calculator } = require("../src/practice");
 
-describe("capitalize tests", () => {
-	test("first test", () => {
-		expect(capitalize("string")).toBe("String");
-		expect(capitalize("word")).toBe("Word");
-		expect(capitalize("WORD")).toBe("WORD");
+// describe("capitalize tests", () => {
+// 	test("first test", () => {
+// 		expect(capitalize("string")).toBe("String");
+// 		expect(capitalize("word")).toBe("Word");
+// 		expect(capitalize("WORD")).toBe("WORD");
+// 	});
+// });
+
+// describe("reverse string", () => {
+// 	test("this function should reverse any given string", () => {
+// 		expect(reverseString("string")).toBe("gnirts");
+// 	});
+// });
+
+describe("calculator object", () => {
+	test("add function", () => {
+		expect(calculator.add(2, 3)).toBe(5);
+	});
+	test("subtract function", () => {
+		expect(calculator.subtract(3, 2)).toBe(1);
+	});
+	test("multiply function", () => {
+		expect(calculator.multiply(2, 2)).toBe(4);
+	});
+	test("divide function", () => {
+		expect(calculator.divide(2, 2)).toBe(1);
 	});
 });
